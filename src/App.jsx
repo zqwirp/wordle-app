e5970c8ccb311139a205366a0179353e526f4cbc
wiji <wiji.permana@gmail.com>
2022-06-27 19:03:14 +0700

    Using JSON Server

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ function App() {
       .then(json => {
         const randomSolution =
           json[Math.floor(Math.random() * json.length - 1)];
+        setSolution(randomSolution);
       });
-  }, []);
+  }, [setSolution]);
 
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
+      {solution && <div>Solution is: {solution.word}</div>}
     </div>
   );
 }
